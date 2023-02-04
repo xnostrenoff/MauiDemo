09bf431853fc8d50384092244667aef6593ae4df
Xavier Nostrenoff <x.nostrenoff@gmail.com>
2023-02-04 18:21:18 +0100

    Implementation of an example to taking picture.

--- MauiDemoApp/MauiDemoApp/MediaTakePicturePage.viewmodel.cs
+++ MauiDemoApp/MauiDemoApp/MediaTakePicturePage.viewmodel.cs
@@ -10,12 +10,34 @@ namespace MauiDemoApp
 {
     public partial class MediaTakePicturePageViewModel : ObservableObject
     {
+        #region Variables
+
+        [ObservableProperty]
+        ImageSource photo;
+
+        #endregion Variables
+
         #region Commands
 
         [RelayCommand]
         async Task GoToBack()
         {
             await Shell.Current.GoToAsync("..");
+        }
+
+        [RelayCommand]
+        async Task TakePhoto ()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult fileResult = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (fileResult != null)
+                {
+                    var stream = await fileResult.OpenReadAsync();
+                    Photo = ImageSource.FromStream(() => stream);                        
+                }
+            }
         }
 
         #endregion Commands
